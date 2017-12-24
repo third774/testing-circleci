6b49b7487045f8fe18a7b6d596316645d5610b98
Kevin Kipp <kevin.kipp@gmail.com>
2017-12-23 20:54:16 -0600

    Testing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          This is deployed with CircleCI!
+          This should not deploy
         </p>
       </div>
     );
