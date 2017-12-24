ad37ac764e63b7c23422ffe87c5bbbac32cda82e
Kevin Kipp <kevin.kipp@gmail.com>
2017-12-23 20:40:58 -0600

    Change text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          IT WORKS! :D
+          This is deployed with CircleCI!
         </p>
       </div>
     );
